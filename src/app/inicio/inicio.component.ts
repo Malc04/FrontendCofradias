1388c97fd18368287fb6160275be907c295f0975
MalcSAFA <mlopezconde@safareyes.es>
2025-06-02 14:44:37 +0200

    2/6 inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  mostrarFormulario: boolean = false;
+  imagenes: string[] = [
+    'png/foto1.png',
+    'png/foto2.png',
+    'png/foto3.png',
+    'png/foto4.png',
+  ];
 
+  imagenSeleccionada: string = this.imagenes[0];
+
+  seleccionarImagen(img: string) {
+    this.imagenSeleccionada = img;
+  }
 }
